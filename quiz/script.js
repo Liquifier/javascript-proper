98da4c4f52be6a22025fdad57ce63c277ac4bde3
Liquifier <infernoliquifier@gmail.com>
2014-05-02 09:07:27 -0700

    quiz resets if no question is selected when the submitAnswer button is pressed
    not sure how to fix that

--- quiz/script.js
+++ quiz/script.js
@@ -1,13 +1,11 @@
+//array of question objects
 var allQuestions = [
     {question: "Who is Prime Minister of the United Kingdom?", choices: ["David Cameron", "Gordon Brown", "Malcom Tucker", "Nicola Murray"], correctAnswer:0},
     {question: "Who is the President of Argentina?", choices: ["Nestor Kirchner", "Christina Fernandez", "Eduardo Dulhalde", "Reynaldo Bignone"], correctAnswer:1},
     {question: "What is the capitol of Australia?", choices: ["Sydney", "Auckland", "Canberra", "Darwin"], correctAnswer:2}
 ];
 
-//basic score tally, probably not even secure
-var score = 0;
-
-//dynamically generate html for next question
+//function to dynamically generate html for next question
 function addNextQuestion (nextQuestion) {
     "use strict";
     var questionContent="";
@@ -19,25 +17,38 @@ function addNextQuestion (nextQuestion) {
     return questionContent;
 }
 
-//load first question after button press
+//event handlers to listen for button press
 $(document).ready(function() {
     "use strict";
-    //initialize the score tally
+    //initialize the score tally using data attribute in the div with the score class
     $("div.score").data('score', 0);
+
+    //listen for startQuiz button click
     $("#startQuiz").click(function(){
+        //remove startQuiz button
         $("div.start").html("");
+
+        //display first question
         $("div.question").html(function() {
             return addNextQuestion(allQuestions[0]);
         });
+
         //set data attribute to keep track of current question number
+        //question number starts at zero to make it easy to reference the allQuestions array
         $("div.question").data("q", 0);
     });
+
+    //listen for subsequent button click events
     $('body').on('click', '#submitAnswer' ,function(){
-        //get answer just submitted
+
+        //get the answer that was selected
         var answer = $("input:radio[name='radAnswer']:checked").val();
+
         //get the question number that was just answered
         var getCurrQ = $("div.question").data("q");
-        if(typeof answer === 'string') { //check for a valid answer before continuing
+
+        //check for a valid answer before continuing
+        if(typeof answer === 'string') {
             if(answer == allQuestions[getCurrQ].correctAnswer) {
                 //get current score so we can add 1 to it after a correct answer
                 var getCurrScore = $('div.score').data('score');
@@ -45,24 +56,23 @@ $(document).ready(function() {
                 alert('Correct! Your current score is ' + $('div.score').data('score'));
             }
             //generate next question if last question has not been loaded
-            //getCurrQ is 0 index based
+            //remember that getCurrQ is 0 index based
             if(getCurrQ < allQuestions.length - 1) {
                 $('div.question').html(function() {
                     return addNextQuestion(allQuestions[getCurrQ+1]);
-                }).data("q", getCurrQ+1);
+                }).data("q", getCurrQ+1); //update data.q to reflect next question loaded
             }
             else {
+                //end of quiz so remove last question
                 $('div.question').html("");
                 var scoreText = "<p>" + "You got " + $('div.score').data('score') + " question(s) out of " + allQuestions.length + " correct!" + "</p>";
+                //generate final score
                 $('div.score').html(function(){
                     return scoreText;
                 });
             }
-
-
-        //implement handling after last question is answered
         } else {
-            //this part resets the page to the original state, don't want that
+            //page just resets after this happens, need to fix
             alert('Please select an answer');
         }
     });
